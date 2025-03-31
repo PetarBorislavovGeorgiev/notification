a50b31427e5f89ccedcf4d0c8f17b3b187eb7dfc
Petar Georgiev <petar.b.georgiev@gmail.com>
2025-03-31 22:25:15 +0300

    Creating the project

---
+++ src/test/java/bg/softuni/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
